7708bf583e2c6b9bfd35de1d1572fee299103c73
loshjarson <jlarson129@gmail.com>
2023-02-19 22:53:52 -0500

    excel shipment date manipulation

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const { BrowserWindow, app, ipcMain } = require("electron");
 const XLSX = require("xlsx")
+const Dayjs = require("dayjs")
 
 require('@electron/remote/main').initialize()
 
@@ -36,13 +37,25 @@ ipcMain.handle('edit-excel', async (event, arg) => {
     //store month of shipment to grab correct sheet 
     const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     const date = arg[0].Shipping_Date.format("MM/DD/YYYY")
-    const monthNum = arg[0].Shipping_Date.getMonth()
+    const monthNum = arg[0].Shipping_Date.get("month")
     const month = months[monthNum]
+    const year = arg[0].Shipping_Date.format(YY)
 
+    //grab excel file
     const workbook = XLSX.readFile("C:/Users/Josh/Downloads/2023 Shipments.xlsx");
 
-    
+    //find range of sheet
     const worksheet = workbook.Sheets[month]
     const range = XLSX.utils.decode_range(worksheet["!ref"])
+    const row = range.e.r + 1
+
+    //grab previous shipment number to find current shipment number
+    if(row > 1){
+        const prevShip = worksheet[XLSX.utils.encode_cell({ r: (row - 1), c: 0})].v 
+        worksheet[XLSX.utils.encode_cell({ r: row, c: 0})].v = prevShip + 1
+    } else {
+        worksheet[XLSX.utils.encode_cell({ r: row, c: 0})].v = year + "001"
+    }
+    
     console.log(range)
 })
