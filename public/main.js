05eb196377ea537c9346d51fe45027410042281a
loshjarson <jlarson129@gmail.com>
2023-02-19 21:26:22 -0500

    began excel edit

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
-const { BrowserWindow, app } = require("electron");
+const { BrowserWindow, app, ipcMain } = require("electron");
+const XLSX = require("xlsx")
 
 require('@electron/remote/main').initialize()
 
@@ -9,7 +10,8 @@ function createWindow() {
         height: 600,
         webPreferences: {
             nodeIntegration: true,
-            enableRemoteModule: true
+            enableRemoteModule: true,
+            contextIsolation: false
         }
     })
     win.loadURL('http://localhost:3000')
@@ -28,4 +30,17 @@ app.on('windows-all-closed', function () {
 app.on('activate', function () {
     //only creates window if there is no window already
     if( BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+ipcMain.handle('edit-excel', async (event, arg) => {
+    //store month of shipment to grab correct sheet 
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    
+
+    const workbook = XLSX.readFile("C:/Users/Josh/Downloads/2023 Shipments.xlsx");
+
+    
+    const worksheet = workbook.Sheets["February"]
+    const range = XLSX.utils.decode_range(worksheet["!ref"])
+    console.log(range)
 })
