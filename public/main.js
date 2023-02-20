98051f6ae15dc4edf87b950c5a4ea143ab674be1
loshjarson <jlarson129@gmail.com>
2023-02-19 22:35:07 -0500

    pragmatically grabs sheet

--- public/main.js
+++ public/main.js
@@ -36,11 +36,13 @@ ipcMain.handle('edit-excel', async (event, arg) => {
     //store month of shipment to grab correct sheet 
     const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     const date = arg[0].Shipping_Date.format("MM/DD/YYYY")
+    const monthNum = arg[0].Shipping_Date.getMonth()
+    const month = months[monthNum]
 
     const workbook = XLSX.readFile("C:/Users/Josh/Downloads/2023 Shipments.xlsx");
 
     
-    const worksheet = workbook.Sheets["February"]
+    const worksheet = workbook.Sheets[month]
     const range = XLSX.utils.decode_range(worksheet["!ref"])
     console.log(range)
 })
